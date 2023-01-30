aee2508d03b41148ee0d614c2c65780d3bf6da18
miishin <miishing@gmail.com>
2023-01-30 00:38:11 -0500

    skeleton of two main pages

---
+++ routes/personal.tsx
@@ -0,0 +1,14 @@
+export default function Personal() {
+    return (
+      <main>
+        <h1>Personal</h1>
+        <ul>
+            <li><a href="https://rateyourmusic.com/~miishin">Music</a></li>
+            <li><a href="https://anilist.co/user/miishin/">AniManga</a></li>
+            <li><a href="https://letterboxd.com/miishin/films/by/shuffle/">Movies</a></li>
+            <li><a href="https://glitchwave.com/user/miishin/">Video Games</a></li>
+            <li><a href="https://www.serializd.com/user/miishin/reviews">Television</a></li>
+        </ul>
+      </main>
+    );
+  }
---
+++ routes/professional.tsx
@@ -0,0 +1,12 @@
+export default function Professional() {
+    return (
+      <main>
+        <h1>Professional</h1>
+        <ul>
+            <li><a href="https://github.com/miishin">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/miraisahara/">LinkedIn</a></li>
+            <li><a href="https://www.flickr.com/people/141681852@N03/">Photography</a></li>
+        </ul>
+      </main>
+    );
+  }
