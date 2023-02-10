89121feb9ba43c234a2a7ad779fe0ca67e272d30
miishin <miishing@gmail.com>
2023-02-09 23:20:25 -0500

    added FLCL colors

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,15 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    colors: {
+      'FLCL': {
+        yellow: '#F6D309',
+        red: '#D8363F',
+        pink: '#FBCBC1',
+        blue: '#294D9F',
+        tan: '#F7DF9C',
+      },
+    }
+  }
 } as Options;
